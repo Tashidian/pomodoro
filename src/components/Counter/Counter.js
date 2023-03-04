fd4efc47a01b9503dad278bcf49aefa57c90d4ad
Tashidian <maria.pena.jaquete@gmail.com>
2023-03-04 20:50:47 +0100

    Funciones de comienzo y reseteo del contador. Al empezar el tiempo va descendiendo hasta llegar a 0, que salta un alert indicando que ha terminado. Al presionar el botón reset el contador vuelve al tiempo inicial

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,19 +1,59 @@
+import { useEffect, useState } from 'react'
 import Button from '../Button/Button'
 import './counter.css'
 
-function Counter() {
+function Counter( props ) {
+
+  const {workMinutes, restMinutes, pomTimes, pomRestMinutes} = props
+
+  const [clockState, setClockState] = useState(0)
+
+  const [clockMinutes, setClockMinutes] = useState(workMinutes)
+  const [clockSeconds, setClockSeconds] = useState(0)
+
   const startButtonClick = () => {
-    console.log('START')
+    setClockState(1)
   }
 
+  useEffect(() => {
+    let intervalo = 0
+    if(clockState === 1) {
+      intervalo = setInterval( () => {
+        if ( clockSeconds === 0 ) {
+          if( clockMinutes === 0 ) {
+            alert('tiempo!')
+            clearInterval(intervalo)
+          } else {
+            setClockMinutes(clockMinutes - 1)
+            setClockSeconds(59)
+          }
+        } else {
+          setClockSeconds(clockSeconds - 1)
+        }
+      }, 1000)
+    }
+    return () => {
+      clearInterval(intervalo)
+    }
+  })
+
   const resetButtonClick = () => {
-    console.log('RESET')
+    setClockMinutes(workMinutes)
+    setClockSeconds(0)
+    setClockState(0)
   }
 
   return (
     <div className="container">
       <div className="counter">
-        <div className="counter__clock">00:00</div>
+        <div className="counter__clock">
+          <span className="counter__clock__minutes">
+            {(clockMinutes < 10) && 0}{clockMinutes}
+          </span>:
+          <span className="counter__clock__seconds">
+            {(clockSeconds < 10) && 0}{clockSeconds}
+            </span>
+          </div>
 
         <div className="counter__buttons">
           <Button type="button" typeAtt="button" css="btn--primary btn--pomodoro" onclick={startButtonClick}>Start</Button>
--- src/components/Pomodoro/Pomodoro.js
+++ src/components/Pomodoro/Pomodoro.js
@@ -1,15 +1,18 @@
+import { useState } from 'react'
 import Counter from '../Counter/Counter'
 import Footer from '../Footer/Footer'
 import Header from '../Header/Header'
 import './pomodoro.css'
 
 function Pomodoro() {
+  const [counterOptions, setCounterOptions] = useState({workMinutes: 25, restMinutes: 5, pomTimes: 4, pomRestMinutes: 30})
+
   return (
     <div className="pomodoro">
       <Header />
 
       <main className="main">
-        <Counter />
+        <Counter workMinutes={counterOptions.workMinutes} restMinutes={counterOptions.restMinutes} pomTimes={counterOptions.pomTimes} pomRestMinutes={counterOptions.pomRestMinutes} />
       </main>
 
       <Footer />
