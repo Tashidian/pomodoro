f29edc66356ca2426cf37ec707204f2c26157768
Tashidian <maria.pena.jaquete@gmail.com>
2023-04-22 16:12:09 +0200

    Añadido un logo al header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ function Header() {
   return (
     <header className="header">
       <div className="container">
+        <img src="logo.svg" alt="Pomodoro logo" className="header__logo" />
         <h1 className="h1">Pomodoro</h1>
       </div>
     </header>
