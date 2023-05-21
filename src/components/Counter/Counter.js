9dd467de52cfa85c543f4867f17832c09bd38dba
Tashidian <maria.pena.jaquete@gmail.com>
2023-05-21 21:22:49 +0200

    Lógica del componente Counter.js movida al hook useTimer

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import useTimer from '../../hooks/useTimer'
 import Alert from '../Alert/Alert'
 import Button from '../Button/Button'
 import Icon from '../Icon/Icon'
@@ -7,91 +7,14 @@ import './counter.css'
 function Counter(props) {
 
   const { workMinutes, shortBreakMinutes, longBreakMinutes, pomTimes } = props
+  const { clockMinutes, clockSeconds, pomState, startPauseTimer, resetTimer, timerMessage } = useTimer(workMinutes, shortBreakMinutes, longBreakMinutes, pomTimes)
 
-  // pomHistory values
-  // 0 - inactive
-  // 1 - work time
-  // 2 - short break
-  // 3 - long break
-
-  const [clockMinutes, setClockMinutes] = useState(workMinutes)
-  const [clockSeconds, setClockSeconds] = useState(0)
-  const [pomState, setPomState] = useState({ isRuning: false, isPaused: false, statusText: 'WORK' })
-  const [pomHistory, setPomHistory] = useState([])
-  const [alertMessage, setAlertMessage] = useState(null)
-
-  const startPauseButtonClick = useCallback(() => {
-    if (!pomState.isRuning) {
-      setPomState({ ...pomState, isRuning: true, isPaused: false })
-
-      if (!pomState.isPaused) {
-        if (pomHistory.length > 0) {
-          if (pomHistory[pomHistory.length - 1] === 1) {
-            (((pomHistory.length + 1) / 2) % pomTimes === 0) ? setPomHistory([...pomHistory, 3]) : setPomHistory([...pomHistory, 2])
-          }
-          if (pomHistory[pomHistory.length - 1] === 2 || pomHistory[pomHistory.length - 1] === 3) {
-            setPomHistory([...pomHistory, 1])
-          }
-        } else {
-          setPomHistory([1])
-        }
-      }
-    } else {
-      setPomState({ ...pomState, isRuning: false, isPaused: true })
-    }
-  }, [pomState, pomHistory, pomTimes])
-
-  const resetButtonClick = useCallback(() => {
-    setClockMinutes(workMinutes)
-    setClockSeconds(0)
-    setPomState({ isRuning: false, isPaused: false, statusText: 'WORK' })
-    setPomHistory([])
-  }, [workMinutes])
-
-  useEffect(() => {
-    let counterInterval = 0
-    if (pomState.isRuning) {
-      counterInterval = setInterval(() => {
-        if (clockSeconds === 0) {
-          if (clockMinutes === 0) {
-            clearInterval(counterInterval)
-
-            if (pomHistory[pomHistory.length - 1] === 1) {
-              if (((pomHistory.length + 1) / 2) % pomTimes === 0) {
-                setClockMinutes(longBreakMinutes)
-                setAlertMessage('Tiempo! Toca descanso largo')
-                setPomState({ statusText: 'LONG BREAK', isRuning: false })
-              } else {
-                setClockMinutes(shortBreakMinutes)
-                setAlertMessage('Tiempo! Toca descanso corto')
-                setPomState({ statusText: 'SHORT BREAK', isRuning: false })
-              }
-            }
-            if (pomHistory[pomHistory.length - 1] === 2 || pomHistory[pomHistory.length - 1] === 3) {
-              setClockMinutes(workMinutes)
-              setAlertMessage('Tiempo! Toca trabajar')
-              setPomState({ statusText: 'WORK', isRuning: false })
-            }
-          } else {
-            alertMessage && setAlertMessage(null)
-            setClockMinutes(clockMinutes - 1)
-            setClockSeconds(59)
-          }
-        } else {
-          setClockSeconds(clockSeconds - 1)
-        }
-      }, 1000)
-    }
-    return () => {
-      clearInterval(counterInterval)
-    }
-  })
 
   return (
     <div className="container">
       <div className="counter">
-        {alertMessage &&
-          <Alert message={alertMessage} sound={true} />
+        {timerMessage &&
+          <Alert message={timerMessage} sound={true} />
         }
         <div className="counter__stage">
           <span className="h2">{pomState.statusText}</span>
@@ -106,10 +29,10 @@ function Counter(props) {
         </div>
 
         <div className="counter__buttons">
-          <Button type="button" typeAtt="button" css="btn--primary btn--pomodoro" onclick={startPauseButtonClick}>
+          <Button type="button" typeAtt="button" css="btn--primary btn--pomodoro" onclick={startPauseTimer}>
             <Icon id={(pomState.isPaused || !pomState.isRuning) ? 'play' : 'pause'} width="50" height="50" />
           </Button>
-          <Button type="button" typeAtt="button" css="btn--secondary" onclick={resetButtonClick}>
+          <Button type="button" typeAtt="button" css="btn--secondary" onclick={resetTimer}>
             <Icon id="stop" width="50" height="50" />
           </Button>
         </div>
---
+++ src/hooks/useTimer.js
@@ -0,0 +1,87 @@
+import { useState, useEffect, useCallback } from 'react'
+
+
+// pomHistory values
+// 0 - inactive
+// 1 - work time
+// 2 - short break
+// 3 - long break
+
+const useTimer = (workMinutes, shortBreakMinutes, longBreakMinutes, pomTimes) => {
+  const [clockMinutes, setClockMinutes] = useState(workMinutes)
+  const [clockSeconds, setClockSeconds] = useState(0)
+  const [pomState, setPomState] = useState({ isRunning: false, isPaused: false, statusText: 'WORK' })
+  const [pomHistory, setPomHistory] = useState([])
+  const [timerMessage, setTimerMessage] = useState(null)
+
+  const startPauseTimer = useCallback(() => {
+    if (!pomState.isRunning) {
+      setPomState({ ...pomState, isRunning: true, isPaused: false })
+
+      if (!pomState.isPaused) {
+        if (pomHistory.length > 0) {
+          if (pomHistory[pomHistory.length - 1] === 1) {
+            (((pomHistory.length + 1) / 2) % pomTimes === 0) ? setPomHistory([...pomHistory, 3]) : setPomHistory([...pomHistory, 2])
+          }
+          if (pomHistory[pomHistory.length - 1] === 2 || pomHistory[pomHistory.length - 1] === 3) {
+            setPomHistory([...pomHistory, 1])
+          }
+        } else {
+          setPomHistory([1])
+        }
+      }
+    } else {
+      setPomState({ ...pomState, isRunning: false, isPaused: true })
+    }
+  }, [pomState, pomHistory, pomTimes])
+
+  const resetTimer = useCallback(() => {
+    setClockMinutes(workMinutes)
+    setClockSeconds(0)
+    setPomState({ isRunning: false, isPaused: false, statusText: 'WORK' })
+    setPomHistory([])
+  }, [workMinutes])
+
+  useEffect(() => {
+    let timerInterval
+    if (pomState.isRunning) {
+      timerInterval = setInterval(() => {
+        if (clockSeconds === 0) {
+          if (clockMinutes === 0) {
+            clearInterval(timerInterval)
+            if (pomHistory[pomHistory.length - 1] === 1) {
+              if (((pomHistory.length + 1) / 2) % pomTimes === 0) {
+                setClockMinutes(longBreakMinutes)
+                setTimerMessage('Tiempo! Toca descanso largo')
+                
+                setPomState({ statusText: 'LONG BREAK', isRuning: false })
+              } else {
+                setClockMinutes(shortBreakMinutes)
+                setTimerMessage('Tiempo! Toca descanso corto')
+                setPomState({ statusText: 'SHORT BREAK', isRuning: false })
+              }
+            }
+            if (pomHistory[pomHistory.length - 1] === 2 || pomHistory[pomHistory.length - 1] === 3) {
+              setClockMinutes(workMinutes)
+              setTimerMessage('Tiempo! Toca trabajar')
+              setPomState({ statusText: 'WORK', isRuning: false })
+            }
+          } else {
+            setClockMinutes(clockMinutes - 1)
+            setClockSeconds(59)
+          }
+        } else {
+          setClockSeconds(clockSeconds - 1)
+        }
+      }, 100)
+    }
+    return () => {
+      clearInterval(timerInterval)
+    }
+  // }, [pomState, clockMinutes, clockSeconds])
+  }, [pomState, clockMinutes, clockSeconds, pomHistory, pomTimes, workMinutes, shortBreakMinutes, longBreakMinutes, timerMessage])
+
+  return { clockMinutes, clockSeconds, pomState, pomHistory, startPauseTimer, resetTimer, timerMessage }
+}
+
+export default useTimer
